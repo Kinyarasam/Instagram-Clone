1ddfe5ed82aaad7034906972ce1360a61101fedf
kinyarasam <samkinyara2000@gmail.com>
2022-06-21 14:21:31 +0300

    MongoDB connected successfully

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ const app = express()
 
 /* CONNECT TO DB */
 const connectDB = require('./db/connect')
+require('dotenv').config()
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
@@ -13,10 +14,13 @@ app.use(express.json())
 
 const PORT = process.env.PORT || 5000
 
+console.log('Establishing Connection . . .\n')
 const start = async () => {
     try {
+        await connectDB(process.env.MONGO_URI)
+        console.log('DATABASE connected SUCCESSFULLY ! ! !')
         app.listen(PORT, () => {
-            console.log(`server is running on PORT ${PORT}`)
+            console.log(`\t Server is running on PORT ${PORT} \n\t url: http://localhost:${PORT}`)
         })
     }catch(err) {
         console.log(err)
