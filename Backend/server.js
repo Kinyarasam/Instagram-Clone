0e9593ced699c78bef60a336daa46b13c88fdc7a
kinyarasam <samkinyara2000@gmail.com>
2022-06-21 14:05:11 +0300

    .

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,26 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT | 5000
 
-app.listen(PORT, () => {
-    console.log(`server is running on PORT ${PORT}`)
+/* CONNECT TO DB */
+const connectDB = require('./db/connect')
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
 })
+app.use(express.json())
+
+const PORT = process.env.PORT || 5000
+
+const start = async () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`server is running on PORT ${PORT}`)
+        })
+    }catch(err) {
+        console.log(err)
+    }
+}
+
+start()
