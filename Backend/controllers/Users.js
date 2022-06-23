e26732ebc200bac8a73b1d7c09ea26bfa0733b55
kinyarasam <samkinyara2000@gmail.com>
2022-06-23 22:29:52 +0300

    Update User(CRUD)

--- Backend/controllers/Users.js
+++ Backend/controllers/Users.js
@@ -35,6 +35,26 @@ const createUser = asyncWrapper(async (req, res, next) => {
 
 // UPDATE USER DETAILS
 const updateUser = asyncWrapper(async (req, res, next) => {
+    try {
+        const { id:userID } = req.params
+        const user = await User.findOneAndUpdate({
+            _id:productID
+        }, req.body, {
+            new: true,
+            runValidators: true
+        })
+
+        if(!user) {
+            return res.status(404).json({
+				msg: `No Product with id: ${ userID }`
+			})
+        }
+
+        res.status(200).json({ product })
+    }catch(err) {
+        res.status(500)
+            .json({ msg: err })
+    }
     console.log('UPDATE USER DETAILS')
 })
 
