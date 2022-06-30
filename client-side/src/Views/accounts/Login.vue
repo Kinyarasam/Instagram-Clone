8efcf0dce3a75e52cfa01086d6ae5759dc1c5582
kinyarasam <samkinyara2000@gmail.com>
2022-06-30 19:44:05 +0300

    Login Vue

--- client-side/src/Views/accounts/Login.vue
+++ client-side/src/Views/accounts/Login.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <div class="bg-white border rounded w-[400px] px-5 mt-10 flex flex-col justify-center">
+    <div class="bg-white border rounded w-[350px] px-10 mt-10 flex flex-col justify-center">
         <div class="flex justify-center my-8">
             <img 
                 src="../../assets/Ig_name_logo.png"  
                 alt=""
-                class="h-10"
+                class="h-[75px]"
             >
         </div>
         <form action="">
@@ -33,19 +33,25 @@
                 </button>
             </div>
         </form>
-        <div class="flex justify-center items-center w-full">
-            <div class="border-b border-black w-full"></div>
+        <div class="flex justify-center items-center w-full font-bold text-slate-400 text-sm">
+            <div class="border-b border-slate-400 w-full"></div>
             <div class="px-3">OR</div>
-            <div class="border-b border-black w-full"></div>
+            <div class="border-b border-slate-400 w-full"></div>
         </div>
-        <div class="flex items-center justify-center text-indigo-900 mt-3">
+        <div class="flex items-center justify-center text-indigo-900 mt-4 cursor-pointer">
             <span class="material-icons">facebook</span>
-            <span> Log in with Facebook</span>
+            <span class="text-[13px] font-medium mx-2"> Log in with Facebook</span>
         </div>
-        Login
+        <div class="font-[12px] my-4 text-indigo-900 cursor-pointer text-xs items-center justify-center w-full flex">
+            Forgot password?
+        </div>
     </div>
-    THIS IS THE LOGIN TEMPLATE
-    <router-link to="/accounts/emailsignup">Sign up</router-link>
+    <div class="flex justify-center items-center bg-white mt-5 border">
+        <span class="text-[13px] my-5">
+            Don't Have an account?
+            <router-link to="/accounts/emailsignup" class="text-blue-500 font-medium">Sign up</router-link>
+        </span>
+    </div>
   </div>
 </template>
 
